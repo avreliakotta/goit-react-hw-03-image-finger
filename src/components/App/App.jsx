dec2f14e183713dc1863e3df99812706b120a9f2
avreliakotta <111601503+avreliakotta@users.noreply.github.com>
2023-09-16 08:56:00 +0300

    Changed function loadMoreImages

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -14,113 +14,94 @@ export class App extends Component {
     page: 1,
     loading: false,
     error: null,
-    hits: null,
+    // hits: null,
     showModal: false,
     selectedHit: null,
     loadMore: true,
-    images:[]
+    images: [],
     
-    }
+    
+  }
 
   componentDidUpdate(prevProps, prevState) {
-    const { page, query } = this.state;
-  console.log('Виконався запит');
+    const { page, query, images } = this.state;
+    console.log('Виконався запит componentDidMount');
     if (page !== prevState.page || query !== prevState.query) {
-      this.setState({ loading: true});
-    fetchPhotos(query,page)
+    
+      this.setState({ loading: true });
+      fetchPhotos(query, page)
         .then((data) => {
-          console.log('data',data);
-          const newImages = page === 1 ? data.hits : [...this.state.images, ...data.hits];
-           this.setState({ images: newImages, loading: false, error: null });
-
-
-          // this.setState({ hits: data.hits, loading: false, error: null });
-        })
-     
-        .catch((error) => {
-          this.setState({ error, loading: false, images: []  });
-        });
-     
-    }
-  }
-  handleSetQuery = (query,page) => {
-      console.log('query',query);
-      this.setState({
-        query,
-        page: 1,
-         });
-    }
-    toggleModal = (selectedHit) => {
-      this.setState(state => ({
-        showModal: !state.showModal,
-        selectedHit
-      }));
-  }
-  loadMoreImages = () => {
-     console.log('loadMoreImages викликано');
-    const { query, page, loading } = this.state;
-  if (loading) {
-      return;
-    }
-
-    const nextPage = page + 1;
-    this.setState({ loading: true });
-
-    fetchPhotos(query, nextPage)
-      .then((data) => {
-        
-        if (data.hits.length > 0) {
-          const newImages = [...this.state.images, ...data.hits];
+          console.log('data', data);
+          const newImages = page === 1 ? data.hits : [...images, ...data.hits];
           this.setState({
             images: newImages,
             loading: false,
-            page: nextPage,
             error: null,
-             loadMore: nextPage < Math.ceil(data.totalHits / 12),
+           loadMore: page+1 < Math.ceil(data.totalHits / 12),
           });
-        } else {
-    
-          this.setState({ loading: false, loadMore: false });
-          console.log("Всі дані завантажено, можна прибрати кнопку");
-        }
-      })
-      .catch((error) => {
-        this.setState({ error, loading: false });
-      });
+    })
+     
+        .catch((error) => {
+          this.setState({ error, loading: false, images: [] });
+        });
+
+    }
+      
   }
-
-
-
-
- 
+    
+  handleSetQuery = (query, page = 1) => {
+    console.log('query', query);
+    this.setState({
+      query,
+      page: 1,
+      images: []
+    });
+  }
+  toggleModal = (selectedHit) => {
+    this.setState(state => ({
+      showModal: !state.showModal,
+      selectedHit
+    }));
+  }
+  loadMoreImages = () => {
+  const { page, loading } = this.state;
+    if (loading) {
+      return;
+    }
+    const nextPage = page + 1;
+    this.setState(prevState => ({
+        loading: false,
+        page: nextPage,
+        error: null,
+       
+      }))
+     }
     render() {
-      const { error, loading,images,hits, showModal, selectedHit } = this.state;
-      console.log('images',images);
+      const { error, loading, images, showModal, selectedHit,loadMore } = this.state;
+     
       return (
         <div className={css.container}>
           <Searchbar onSubmit={this.handleSetQuery} />
           <ImageGallary images={images} openModal={this.toggleModal} />
         
-          <Button action={this.loadMoreImages} />
+          { images.length > 0 && loadMore && <Button action={this.loadMoreImages} />}
          
-            
-
           {loading && (
-          <ThreeDots
-            height="80"
-            width="80"
-            radius="9"
-            color='tomato'
-            ariaLabel="three-dots-loading"
-            wrapperStyle={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignItems: 'center'
-            }}
-            wrapperClassName={css.loader}
-            visible={loading}
-          />
-        )}
+            <ThreeDots
+              height="80"
+              width="80"
+              radius="9"
+              color='tomato'
+              ariaLabel="three-dots-loading"
+              wrapperStyle={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+              wrapperClassName={css.loader}
+              visible={loading}
+            />
+          )}
          
           {showModal && <Modal selectedHit={selectedHit} onClose={this.toggleModal} />}
          
